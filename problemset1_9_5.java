6c574f732b4cfd7ddd342653b10db034ea3d40e7
Amy Zhou <amy_zhou@m018652.aw.org>
2024-09-06 14:00:12 -0700

    Homework Sep5th

---
+++ problemset1_9_5.java
@@ -0,0 +1,15 @@
+class problemset1 {
+
+public static void main(String[] args) {
+
+	System.out.println("Amy" + " " + "is a student in IB CS.");
+
+	System.out.println("Python is dynamically typed and Java is statically typed. Java is universal than python.");
+
+	System.out.println("Assembly language consists of 0s and 1s. HLL uses English like statements for operations.");
+	
+	System.out.println("Programmers use compilers to translate HLL into machine code that computers can understand.");
+
+}
+
+}

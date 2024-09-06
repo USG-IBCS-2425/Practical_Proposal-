2402b14eca31c65749c1380dae6bcdfda972addf
Amy Zhou <amy_zhou@m018652.aw.org>
2024-09-06 13:50:02 -0700

    note Sep4th

---
+++ note9_4.java
@@ -0,0 +1,15 @@
+class notes9_4 {
+	//public is required so that this method
+	//canbe accessed outside of the class
+	//static means that the method stays the same
+	//for all instances of the class
+	//void means no output
+	//string[] args is the paramenter
+
+public static void main(String[] args) {
+
+	System.out.println("Hello World!");
+	
+}
+
+}

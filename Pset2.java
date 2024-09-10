c849a3ac061341de13d38d509452bd11f75740da
Amy Zhou <amy_zhou@m018652.aw.org>
2024-09-10 08:08:32 -0700

    hi

---
+++ Pset2.java
@@ -0,0 +1,75 @@
+class Pset {
+
+	public static void main(String[] args) {
+
+		String name = "Amy";
+
+		String date = "Sep 9th";
+
+		System.out.println(name + " " + date);
+
+		System.out.println(" ");
+
+		System.out.println(" ");
+
+		int x = 9;
+
+		System.out.println(x + " " + "students are in our class");
+
+		int y = x + 1;
+
+		System.out.println(y + " " + "students are in our class");
+
+		if(y < 4) {
+
+		System.out.println("the class is cancelled.");
+
+	} else {
+
+		System.out.println("the class will continue.");
+
+	}
+
+		int v = 3;
+
+		int z = y - v;
+
+		if(z < 4) {
+
+		System.out.println("the class is cancelled.");
+
+	} else {
+
+		System.out.println("the class will continue.");
+
+	}
+
+		boolean devisible = true;
+
+ 		int r = x % 3;
+
+ 		int n = x/3; 
+
+ 		int o = 3 - r;
+
+ 		if(r == 0) {
+
+ 		devisible = true;
+
+  		System.out.print(devisible);
+
+ 		System.out.print(n);
+
+
+ 		} else {
+
+  		devisible = false;
+
+  		System.out.println(o);
+
+ }
+
+ }
+
+}
+

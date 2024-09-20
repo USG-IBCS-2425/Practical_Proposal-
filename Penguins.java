1b0cfe5856302638469f900c9fc35ac4c1b46e44
Amy Zhou <amy_zhou@m018652.aw.org>
2024-09-20 13:43:52 -0700

    zoo project

---
+++ Penguins.java
@@ -0,0 +1,60 @@
+class Penguins {
+
+	static String animals = "";
+
+	static int animalsnum = 0;
+
+	public static void main(String[] args) {
+
+		Penguin a = new Penguin("Apple",5);
+		addAnimal(a);
+
+		Penguin b = new Penguin("Banana",5);
+		addAnimal(b);
+
+		Penguin c = new Penguin("coconut",5);
+		addAnimal(c);
+
+		Penguin d = new Penguin("durian",5);
+		addAnimal(d);
+
+		Penguin f = new Penguin("fig",5);
+		addAnimal(f);
+
+		System.out.println("Penguins in the enclosure: " + getAnimal());
+		System.out.println("There are " + countAnimals() + " penguins in the enclosure.");
+
+	}
+
+	public static void addAnimal(Penguin p) {
+
+        if (animals.isEmpty()) {
+
+            animals = p.getName(); 
+
+        } else {
+
+            animals = animals + ", " + p.getName(); 
+
+        }
+
+        animalsnum++;	
+
+    }
+
+	public static String getAnimal() {
+        return animals; 
+    }
+
+    public static int countAnimals() {
+        return animalsnum;
+    }
+
+
+
+
+
+
+
+
+}
